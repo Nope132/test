6757ab3a507dc0c28b5a5015f4bd40eabf739038
Nope132 <156041599+Nope132@users.noreply.github.com>
2024-09-06 14:37:43 +0800

    Add files via upload

---
+++ src/views/Main.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+          <el-container>
+              <el-aside width="200px">
+                <common-aside />
+              </el-aside>
+              <el-container>
+                <el-header>Header</el-header>
+                <el-main>
+                  <router-view></router-view>
+                </el-main>
+              </el-container>
+          </el-container>
+    </div>
+</template>
+
+<script>
+import CommonAside from '../components/CommonAside.vue'
+export default{
+  data(){
+    return {}
+  },
+  components: {
+    CommonAside
+  }
+}
+</script>
---
+++ src/views/User.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>I'm User</h1>
+</template>
+<script>
+export default{
+  data(){
+    return {}
+  }
+}
+</script>
